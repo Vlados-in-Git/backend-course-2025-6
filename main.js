3dbb6eb7cbad46276e8b26b755a2950ef6dbf44c
Vlados-in-Git-bc2025-6 <3vlad2017111111111@gmail.com>
2025-11-22 21:34:47 +0200

    Make command -h, -p, -c

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
+const {program} = require ('commander');
+const fs = requre('fs').promises;
+const fss = requre('fs');
+const path = requre('path');
+const http = requre('http');
+const superagent = require ('superagent');
 
-console.log("Hello World...")
+program
+	.requiredOption('-h, --host <host>', 'адреса сервера')
+	.requiredOption('-p, --port <port>', 'порт сервера')
+	.requiredOption('-c, --cache <path>', 'шлях до директорії з кешом');
+
+program.parse(process.argv);
+const options = program.opts();
+const host = options.host;
+const port = options.port;
+const cacheDir = path.resolve(options.cache);
+
+if(!fss.existsSync(cacheDir)) {
+
+	console.log(`Директорія кешу не існує. Створюємо ${cacheDir}`);
+	fss.mkdirSync(cacheDir, { recursive: true});
+	}else {
+	console.log(`Директорія кешу існує: ${cacheDir}`);
+	}
+
+
+
